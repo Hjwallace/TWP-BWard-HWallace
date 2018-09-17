2b3d66890f136a4a969704dc33a5f79dea33776c
Lieren <hunterjwallace@gmail.com>
2018-09-17 11:51:22 -0400

    Day One
    Need to fix test data/input stream

---
+++ Project/src/test/java/edu/bsu/cs222/testJSONData.java
@@ -0,0 +1,31 @@
+package edu.bsu.cs222;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import jdk.nashorn.internal.parser.JSONParser;
+import jdk.nashorn.internal.parser.Parser;
+import org.junit.Assert;
+import org.junit.Test;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+
+public class testJSONData {
+
+    @Test
+    public void testReadFirstUser(){
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("testingData.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        //JsonObject rootObject = rootElement.getAsJsonObject();
+        //JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        //Assert.assertNotNull(pages);
+        Assert.assertNotNull(rootElement);
+
+    }
+
+}
