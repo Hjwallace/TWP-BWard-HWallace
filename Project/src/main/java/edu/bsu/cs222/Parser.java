138c4a6ebeb9d4241f6ccc4b823a8cc61fe09e3c
Lieren <hunterjwallace@gmail.com>
2018-09-22 16:31:56 -0400

    Working on Parsing Data
    Worked more on parsing and making solid progress. Need to work on the parser.java and parse test which atm runs red. Seems to be an issue with importing, I have left comments to any changes made.

--- Project/src/main/java/edu/bsu/cs222/Parser.java
+++ Project/src/main/java/edu/bsu/cs222/Parser.java
@@ -1,10 +1,37 @@
 package edu.bsu.cs222;
 
+import org.w3c.dom.*; //.Nodelist
+import org.w3c.dom.Document; //Added
+
+import org.xml.sax.SAXException;
+
+import javax.swing.text.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public List<Revision> parse(InputStream input) {
-        return null;
+
+    public List<Revision> parse(InputStream input) throws IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = null;
+
+        try {
+            documentBuilder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new IOException(e);
+        }
+
+        Document document = documentBuilder.parse(input);
+        NodeList nodeList = ((org.w3c.dom.Document) document).getElementsByTagName("rev");
+        List<Revision> result = new ArrayList<>();
+        for (int i = 0; i<nodeList.getLength();i++){
+            result.add(new Revision());
+        }
+        return result;
     }
 }
--- Project/src/test/java/edu/bsu/cs222/ParserTest.java
+++ Project/src/test/java/edu/bsu/cs222/ParserTest.java
@@ -3,17 +3,22 @@ package edu.bsu.cs222;
 import jdk.internal.util.xml.impl.Input;
 import org.junit.Assert;
 import org.junit.Test;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
 public class ParserTest {
 
+
+    //STATUS: Returning Null/not being able to parse through any data
     @Test
-    public void testParse(){
+    public void testParse() throws IOException, SAXException {
         Parser parse = new Parser();
-        InputStream sampleInput = getClass().getClassLoader().getResourceAsStream("testData.json");
+        InputStream sampleInput = getClass().getClassLoader().getResourceAsStream("testingData.json");
         List<Revision> revisions = parse.parse(sampleInput);
+        System.out.println(revisions);
         Assert.assertEquals(4, revisions.size());
 
     }
