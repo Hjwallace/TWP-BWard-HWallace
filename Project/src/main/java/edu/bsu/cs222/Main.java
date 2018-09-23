1a043fc92cfcc6f83e07ef7869d4f834bfb6a680
Lieren <hunterjwallace@gmail.com>
2018-09-23 16:17:48 -0400

    UI start
    Preparing UI and learning how to format it

--- Project/src/main/java/edu/bsu/cs222/Main.java
+++ Project/src/main/java/edu/bsu/cs222/Main.java
@@ -1,19 +1,39 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainUI.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root,300,275));
+    public void start(Stage primaryStage) throws Exception {
+        VBox parent = new VBox();
+        parent.getChildren().add(new Label("Hello World"));
+
+        HBox urlArea = new HBox(new Label("URL:"));
+        TextField textField = new TextField();
+        urlArea.getChildren().add(textField);
+
+        parent.getChildren().add(urlArea);
+
+        Button button = new Button("Press me!");
+        button.setOnAction(event -> System.out.println("You pressed me"));
+        parent.getChildren().add(button);
+
+        //Scene scene = new Scene(new Label("Foo"));
+        primaryStage.setScene(new Scene(parent));
         primaryStage.show();
+
     }
-    public static void main(String[] args) {launch(args);}
 }
