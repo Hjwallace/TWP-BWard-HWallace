8fc4d8ac52ce74f4029b230df96ed8bf0f9cebd2
Bryson Ward <blward@bsu.edu>
2018-09-24 20:37:35 -0400

    Time Zone Converter code but.. Throws an error in main

---
+++ Project/src/main/java/edu/bsu/cs222/TimeZoneConverter.java
@@ -0,0 +1,29 @@
+package edu.bsu.cs222;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class TimeZoneConverter {
+    public TimeZoneConverter() {
+    }
+
+    public static void main(String[] args) {
+        Calendar universalTime = new GregorianCalendar(TimeZone.getTimeZone("Universal"));
+        int hour = universalTime.get(18);
+        int minute = universalTime.get(30);
+        int second = universalTime.get(23);
+        int year = universalTime.get(1);
+
+        System.out.printf("Universal time: %d: %d: %d %d\n", hour, minute, second, year);
+
+        Calendar IndianapolisTime = new GregorianCalendar(TimeZone.getTimeZone("America/Indianapolis"));
+        IndianapolisTime.setTimeInMillis(universalTime.getTimeInMillis());
+        hour = IndianapolisTime.get(18);
+        minute = IndianapolisTime.get(30);
+        second = IndianapolisTime.get(23);
+        year = IndianapolisTime.get(1);
+
+        System.out.printf("Eastern time: %d: %d: %d %d\n", hour, minute, second, year);
+    }
+}
